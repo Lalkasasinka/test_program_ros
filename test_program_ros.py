59831280db88d4ff8ef0ced14e833050506bca9a
Иван Синицын <trololo201.lo@mail.ru>
2024-12-05 08:21:57 +0300

    All good

--- test_program_ros.py
+++ test_program_ros.py
@@ -1,22 +1,6 @@
 import yaml
 
 
-# Преобразование данных скоростей в скорости вращения и углы поворота
-def convert_speeds(linear_speed, angular_speed):
-    rotation_speed = 0
-    rotation_angle = 0
-    # Если робот не стоял на месте
-    if angular_speed != 0:
-        # Находим радиус
-        wheel_radius = linear_speed / angular_speed
-        # Находим скорость вращения
-        rotation_speed = linear_speed / wheel_radius
-        # Находим угол поворота
-        rotation_angle = angular_speed * wheel_radius
-        return rotation_speed, rotation_angle
-    return rotation_speed, rotation_angle
-
-
 # Чтение данных yaml и преобразование в python-объекты
 def read_input(filename):
     with open(filename, 'r') as f:
@@ -30,19 +14,53 @@ def write_output(filename, data):
         f.write('---\n')
 
 
+# Находим скорость варщение и угол поворота
+def convert_speeds(data, time):
+    
+    #Извлекаем линейную и угловую скорости
+    linear_speed = data['twist']['linear']['x']
+    angular_speed = data['twist']['angular']['z']
+
+    # Рассчитываем скорость вращения приводного колеса
+    rotation_speed = 0
+    if angular_speed != 0:
+        r = linear_speed / angular_speed
+        rotation_speed = linear_speed / r
+
+    # Рассчитываем угол поворота за это время
+    rotation_angle = angular_speed * time
+
+    return rotation_speed, rotation_angle
+
+
+# Вычисление разницы во времени между двумя временными метками
+def calc_time(start_stamp, end_stamp):
+    start_time = start_stamp
+    end_time = end_stamp
+    time = end_time - start_time
+    return time
+
+
 def main():
-    input_data = read_input('input_lite_example.yaml')
+    input_data = read_input('inp.yaml')
     output_data = []
-    for obj in input_data:
-        linear_speed = obj['twist']['linear']['x']
-        angular_speed = obj['twist']['angular']['z']
-        rotation_speed, rotation_angle = convert_speeds(linear_speed,
-                                                        angular_speed)
+    for i in range(len(input_data)-1):
+        time = 0
+        if input_data[i+1] is not None and input_data[i]['twist']['angular']['z'] != 0.0:
+
+            # Рассчитываем разницу во времени между текущей и следующей меткой
+            time = calc_time(input_data[i]['header']['stamp']
+                             ['secs'] + (input_data[i]['header']['stamp']
+                             ['nsecs'])/1e9, input_data[i+1]['header']['stamp']
+                             ['secs'] + (input_data[i+1]['header']['stamp']
+                                         ['nsecs']) / 1e9)
+         
+        rotation_speed, rotation_angle = convert_speeds(input_data[i], time)
         output_data.append({
             'linear': {
                 'x': rotation_speed,
                 'y': 0.0,
-                'z': 0.0
+                'z': 0.0,
             },
             'angular': {
                 'x': 0.0,
@@ -51,8 +69,8 @@ def main():
             }
         })
 
-    # Запись преобразованных данных в output_lite.yaml
-    write_output('output_lite.yaml', output_data)
+        # Запись преобразованных данных в output_lite.yaml
+        write_output('output_lite.yaml', output_data)
 
 
 if __name__ == '__main__':
